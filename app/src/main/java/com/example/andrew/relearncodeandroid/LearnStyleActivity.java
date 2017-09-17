ff81df06e379280dd152fb9ad06d6d5539f56f7d
andrewananta <andrew@traveloka.com>
2017-09-18 01:12:45 +0700

    1. add gradient & shadow to textView's

--- app/src/main/java/com/example/andrew/relearncodeandroid/LearnStyleActivity.java
+++ app/src/main/java/com/example/andrew/relearncodeandroid/LearnStyleActivity.java
@@ -1,7 +1,11 @@
 package com.example.andrew.relearncodeandroid;
 
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LearnStyleActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class LearnStyleActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_learn_style);
+
+    TextView textViewGradient = (TextView)findViewById(R.id.txtViewGradient);
+    LinearGradient gradient1 = new LinearGradient(0, 0, textViewGradient.getTextSize(), 0 , Color.GREEN, Color.RED, Shader.TileMode.MIRROR);
+    textViewGradient.getPaint().setShader(gradient1);
   }
 }
